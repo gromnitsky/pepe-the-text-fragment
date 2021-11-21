de9b0b0ddc77ea02370ebd95e865215c116fc6f4
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2021-11-21 04:11:45 +0200

    simplify

--- pepe-the-text-fragment.js
+++ pepe-the-text-fragment.js
@@ -1,13 +1,12 @@
-function draw(z_index) {
-    let d = document.createElement('div')
-    d.style.cssText = `position:absolute;top:0;left:0;height:100vh;width:100vw;z-index:${z_index};display:flex;justify-content:center;align-items:center;background:rgba(0,0,0,0.2);`
-    d.innerHTML = '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAQBAMAAADkNkIoAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAhUExURUdwTKGumFiNPnyQbI1VL8LHulp3N1plU3RhMQAAAP///2sfVBEAAAABdFJOUwBA5thmAAAAfklEQVQI12NgYGAINg5gFWUAAUYlJWUlJQEQM0gJBBRATCMwUzkALA8GqgwMQkCqWNiwXJCBFSgnOD1w6fRSsLx6eZKymQADE1SpIkQp2ASYUUogO4DMFJe2tLS0ApCCFhcQEGBgVlJSS+vocHFkgOlTA9rLwBoobGxsCGQBAOepGb3TWKDyAAAAAElFTkSuQmCC">'
-    d.querySelector('img').style.cssText = 'image-rendering:pixelated;max-height:100%;max-width:100%;height:500px;width:auto;'
-    document.body.appendChild(d)
-    document.querySelector('body').style = 'overflow: hidden'
-}
-
 export default function(z_index = 999) {
     let url = performance.getEntries().find( v => v.type === 'navigate')?.name || location.href;
-    /#[^:~]*?:~:text=/.test(url) && draw(z_index)
+    /#.*:~:text=/.test(url) && draw(z_index)
+}
+
+function draw(z_index) {
+    let d = document, w = d.createElement('div')
+    w.style.cssText = `position:fixed;top:0;left:0;height:100%;width:100%;z-index:${z_index};display:flex;justify-content:center;align-items:center;background:rgba(0,0,0,.2)`
+    w.innerHTML = '<img src="data:image/gif;base64,R0lGODlhFAAQAOMKAAAAAFp3N3RhMVplU41VL1iNPnyQbKGumMLHuv////b27/b27/b27/b27/b27/b27yH5BAEKAA8ALAAAAAAUABAAAARl8MmJjEUSHTz7O0UYGmJxeFmpiuhDrqrBSSAMc7U6GId1DIPDCRFLHAADTQKBTH5gwEFgZAicHjbbNZctGSSvruoUDgUIaEFgzQ4MniUBek5HX8sFtmAvt3e4NgEzFBo9FocbHhEAOw==">'
+    w.querySelector('img').style.cssText = 'image-rendering:pixelated;max-height:100%;max-width:100%;height:500px;width:auto'
+    d.body.appendChild(w)
 }
